6a64b21a97333b06e7490187e6cf3088f19de40c
V. Molinari - mac <there.is.no.spoon.2159@gmail.com>
2025-03-22 10:09:42 +0100

    alembic improvement

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,12 @@
 # backend/app/database.py
+from getpass import getuser
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.config import DATABASE_NAME, DATABASE_PORT
 
 # Configura l'URL del database
-DATABASE_URL = f"postgresql://mr.anderson2159@localhost:{DATABASE_PORT}/{DATABASE_NAME}"
+DATABASE_URL = f"postgresql://{getuser()}@localhost:{DATABASE_PORT}/{DATABASE_NAME}"
 
 # Crea l'engine per la connessione al database
 engine = create_engine(DATABASE_URL)
